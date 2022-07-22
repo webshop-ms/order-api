689fbd0b980b34c0b571baa91e9cee53cd5d3b02
Szabolcs Torma <szabolcs.torma@accretivetg.com>
2022-07-22 16:37:40 +0200

    feign client settings

--- src/main/java/com/webshop/order/OrderApplication.java
+++ src/main/java/com/webshop/order/OrderApplication.java
@@ -1,13 +1,18 @@
 package com.webshop.order;
 
+import com.webshop.common.exceptionhandling.ValidationExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@EnableFeignClients
+@Import(ValidationExceptionHandler.class)
 public class OrderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
 
 }
--- src/main/java/com/webshop/order/proxy/ItemProxy.java
+++ src/main/java/com/webshop/order/proxy/ItemProxy.java
@@ -1,2 +1,16 @@
-package com.webshop.order.proxy;public interface ItemProxy {
+package com.webshop.order.proxy;
+
+import com.webshop.common.model.dto.ItemDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name = "item-service", url = "localhost:9000/api")
+public interface ItemProxy {
+
+    @GetMapping("/items/find-all-by")
+    public List<ItemDto> getAllItemsById(@RequestParam(name = "uuids") List<String> ids);
+
 }
