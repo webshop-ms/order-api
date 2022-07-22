463d3c556acf1bfab08d5abe2eb21d23566e827f
Szabolcs Torma <szabolcs.torma@accretivetg.com>
2022-07-22 16:53:07 +0200

    validate path variable

--- src/main/java/com/webshop/order/controller/OrderController.java
+++ src/main/java/com/webshop/order/controller/OrderController.java
@@ -1,19 +1,23 @@
 package com.webshop.order.controller;
 
 import com.webshop.common.model.dto.OrderDto;
+import com.webshop.common.model.entity.Identity;
 import com.webshop.order.facade.OrderFacade;
 import com.webshop.order.model.request.OrderCreateRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/orders")
 @AllArgsConstructor
+@Validated
 public class OrderController {
 
     private OrderFacade orderFacade;
@@ -32,13 +36,13 @@ public class OrderController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<OrderDto> getOrder(@RequestHeader(name = "id") String uuid) {
+    public ResponseEntity<OrderDto> getOrder(@PathVariable(name = "id") @Pattern(regexp = Identity.UUID_REGEX) String uuid) {
         OrderDto orderDto = orderFacade.getOrder(uuid);
         return new ResponseEntity<>(orderDto, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteOrder(@RequestHeader(name = "id") String uuid) {
+    public ResponseEntity<Void> deleteOrder(@PathVariable(name = "id") @Pattern(regexp = Identity.UUID_REGEX) String uuid) {
         orderFacade.deleteOrderByUuid(uuid);
         return new ResponseEntity<>(HttpStatus.OK);
     }
